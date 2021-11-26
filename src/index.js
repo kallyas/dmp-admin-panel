1c560bcc6583fc9bb870d338ff6e0e4f67aaabb3
kallyas <kallyasmedia@gmail.com>
2021-11-26 07:43:07 +0000

    Init v2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App';
-import * as serviceWorker from './serviceWorker';
-import './index.css'
+import './index.css';
+import App from './App';
 
-export default function Root() {
-  ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-Root();
-
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./app/App', Root);
-}
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
--- src/theme/Colors.js
+++
@@ -1,5 +0,0 @@
-export const Colors = {
-    primary: '#F7F8F8',
-    secondary: '#F3F8FB',
-    tertiary: '#A2302F'
-};
