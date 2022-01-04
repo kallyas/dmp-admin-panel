e5a9388f1d69d17cce312e28e2902cd3a410bc94
kallyas <kallyasmedida@gmail.com>
2022-01-04 11:01:31 +0000

    ft: add favicon and dashboard data

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,29 @@ import Card from "./Card";
 import Trips from "./charts/trips";
 
 const Main = () => {
+  let cardData = [
+    {
+      title: "Total Trips",
+      value: "120",
+    },
+    {
+      title: "Total Bus Operators",
+      value: "10",
+    },
+    {
+      title: "Total Staff",
+      value: "2",
+    },
+    {
+      title: "Total Vehicles",
+      value: "23",
+    }
+  ]
   return (
     <div className="row row-deck row-cards">
-      {[1, 2, 3, 4].map((i) => (
+      {cardData.map((data, i) => (
         <div key={i} className="col-sm-6 col-lg-3">
-          <Card title="Card title" count={100} />
+          <Card title={data.title} count={data.value} />
         </div>
       ))}
       <div className="col-lg-6">
